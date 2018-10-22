621fd67ab9e7a85af7b98df2528152cdadd6325e
yoli24 <43812870+yoli24@users.noreply.github.com>
2018-10-22 20:56:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,82 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+const prefix = '!';
+const help = '!total, !day';
+var guildID = '326996219782234115';
+var channelIDS = ['438405492612792331','443111510764552215','447860504174657546'];
+var usersIDS = [];
+var data = {};
+const tickTimeSpan = 10000; //10 seconds
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+function TimeTick(){
+    var guild = bot.guilds.find("id", guildID);
+    guild.members.forEach(function(elem){
+        if(elem.voiceChannel!=null){
+            for(var i=0;i<channelIDS.length;i++){
+                if(elem.voiceChannel.id==channelIDS[i]){
+                    if(!usersIDS.includes(elem.id)){
+                        usersIDS.push(elem.id);
+                    }  
+                    if(!data[elem.id]){
+                        data[elem.id]=0;
+                    }
+                    data[elem.id]+=tickTimeSpan/1000;
+                    
+                    break;
+                }
+            }
+        }
+    });
+}
+
+bot.on('ready', async()=>{
+    console.log("\x1b[42m%s\x1b[0m", `Connected to ${bot.user.tag}!`);
+    bot.user.setActivity("test");
+    TimeTick();
+    bot.setInterval(TimeTick, tickTimeSpan);
+
 });
+bot.on('message', (message)=>{
+    if(!(message.channel.type=="dm")) return;
+    if(message.author.id=='265205555075743754') return;
+    if(message.author.bot) return;
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+    if(!message.content.startsWith(prefix)){
+        message.channel.send(help);
+        return;
+    }
+    else{
+        var cmd = message.content.split(' ')[0];
+        switch(cmd){
+            case prefix+"day":
+            var x = [];
+            var i, name;
+            var timeType, time;
+            while(usersIDS.length>0){
+            i = usersIDS.pop();
+            name = bot.users.find('id', i);
+            time = data[i];
+            
+            timeType="seconds";
+            if(time>60){
+                time=time/60;
+                timeType="minutes";
+                if(time>60){
+                    time=time/60;
+                    timeType="hours";
+                }
+            }
+            time=Math.floor(time);
+            message.channel.send(name+" time: "+time+" "+timeType);
+            x.push(i);
+            }
+            usersIDS=x;
+            break;
+            case prefix+"total":
+
+            break;
+        }
+    }
+ 
 });
-
 client.login(process.env.BOT_TOKEN);
