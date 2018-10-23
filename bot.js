ec51b4dc38bbdbf1802c349868a410a3b86f7a41
yoli24 <43812870+yoli24@users.noreply.github.com>
2018-10-23 08:56:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,10 +18,8 @@ function CheckDate(){
     }
 }
 function TimeTick(){
-    CheckDate();
     var guild = bot.guilds.find("id", guildID);
     guild.members.forEach(function(elem){
-        if(elem
         if(elem.voiceChannel!=null){
             for(var i=0;i<channelIDS.length;i++){
                 if(elem.voiceChannel.id==channelIDS[i]){
@@ -37,7 +35,7 @@ function TimeTick(){
                 }
             }
         }
-         else if(elem.id=='242360233593274369'){
+        else if(elem.id=='242360233593274369'){
             if(!usersIDS.includes(elem.id)){
                 usersIDS.push(elem.id);
             }  
